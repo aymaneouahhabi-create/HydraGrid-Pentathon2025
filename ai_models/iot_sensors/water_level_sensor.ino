e998bca190098e0fed15940ed86a0bc32d6174cc
aymaneouahhabi-create <aymane.ouahhabi.ensao@ump.ac.ma>
2025-11-27 21:29:49 +0100

    Create water_level_sensor.ino

---
+++ ai_models/iot_sensors/water_level_sensor.ino
@@ -0,0 +1,10 @@
+// Code pour capteur ultrason (ESP32)
+// Communication via LoRaWAN vers le serveur central
+void setup() {
+  Serial.begin(9600);
+  // Initialisation des capteurs
+}
+void loop() {
+  // Lecture du niveau d'eau
+  // Envoi de l'alerte si seuil critique dépassé
+}
