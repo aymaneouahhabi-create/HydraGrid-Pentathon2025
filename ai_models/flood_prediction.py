5e0660964f767b52dd955f280ebea6331378d5e5
aymaneouahhabi-create <aymane.ouahhabi.ensao@ump.ac.ma>
2025-11-27 21:29:19 +0100

    Create flood_prediction.py

---
+++ ai_models/flood_prediction.py
@@ -0,0 +1,6 @@
+# Module de prédiction des inondations (Hydra)
+# TODO: Implémenter le modèle LSTM pour les données pluviométriques
+# Input: Données historiques Oued Isly
+import tensorflow as tf
+import pandas as pd
+print("Initialisation du modèle Hydra...")
